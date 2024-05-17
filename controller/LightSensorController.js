ac72dd87c267a1ed3b7221cb6c6ff84a091d2596
Tarho <20521935@gm.uit.edu.vn>
2024-05-18 01:07:32 +0700

    update again nè

---
+++ controller/LightSensorController.js
@@ -0,0 +1,93 @@
+import LightSensor from "../models/sensor_light.js";
+import Board from "../models/board.js";
+
+export const GetLightValue = async (req, res, next) => {
+  try {
+    const board_id = req.params.board_id;
+    const latestLight = await LightSensor.findOne({ board_id })
+      .sort({ _id: -1 })
+      .limit(1);
+
+    if (!latestLight) {
+      return res.status(404).json({ message: "Light value not found" });
+    }
+
+    const board = await Board.findById(board_id); // Assuming Board is a Mongoose model
+    if (!board) {
+      return res.status(404).json({ message: "Board not found" });
+    }
+
+    const { board_name, ipaddress } = board;
+
+    const { light, date } = latestLight;
+
+    const response = {
+      board_name,
+      ipaddress,
+      board_id,
+
+      light,
+      date,
+    };
+    return res.status(200).json(response);
+  } catch (error) {
+    console.error("Error fetching humidity value:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const getAllBoards = async (req, res, next) => {
+  try {
+    const boards = await Board.find();
+    return res.status(200).json(boards);
+  } catch (error) {
+    console.error("Error fetching board data:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+// POST
+export const AddLightSensorValue = async (req, res, next) => {
+  try {
+    const { light, board_id } = req.body;
+    const board = await Board.findOne({ _id: board_id });
+
+    if (!board) {
+      return res.status(404).json({ success: false, error: "Board not found" });
+    }
+
+    const { board_name, ipaddress } = board;
+
+    const currentTime = new Date();
+    const offset = 7 * 60;
+    currentTime.setMinutes(currentTime.getMinutes() + offset);
+
+    const lightsensor = new LightSensor({
+      board_name,
+      ipaddress,
+      board_id,
+      light,
+      date: currentTime,
+    });
+
+    // Save the lightsensor
+    await lightsensor.save();
+
+    return res.status(200).json({ success: true, sensor: lightsensor });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ success: false, error: "Internal server error" });
+  }
+};
+
+export const getAllDataL = async (req, res, next) => {
+  try {
+    const allData = await LightSensor.find();
+    return res.status(200).json(allData);
+  } catch (error) {
+    console.error("Error fetching all data:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
---
+++ controller/TempurateController.js
@@ -0,0 +1,115 @@
+import Board from "../models/board.js";
+import TemperatureSensor from "../models/sensor_tempurate.js";
+
+export const getHumidity = async (req, res, next) => {
+  try {
+    const board_id = req.params.board_id;
+    const latestHumidity = await TemperatureSensor.findOne({ board_id })
+      .sort({ _id: -1 })
+      .limit(1);
+
+    if (!latestHumidity) {
+      return res.status(404).json({ message: "Humidity value not found" });
+    }
+
+    const board = await Board.findById(board_id); // Assuming Board is a Mongoose model
+    if (!board) {
+      return res.status(404).json({ message: "Board not found" });
+    }
+
+    const { board_name, ipaddress } = board;
+
+    const { humidity, date } = latestHumidity;
+
+    const response = {
+      board_name,
+      ipaddress,
+      board_id,
+      humidity,
+      date,
+    };
+    return res.status(200).json(response);
+  } catch (error) {
+    console.error("Error fetching humidity value:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const getTemperature = async (req, res, next) => {
+  try {
+    const board_id = req.params.board_id;
+    const latestTemperature = await TemperatureSensor.findOne({ board_id })
+      .sort({ _id: -1 })
+      .limit(1);
+
+    if (!latestTemperature) {
+      return res.status(404).json({ message: "Temperature value not found" });
+    }
+
+    const board = await Board.findById(board_id); // Assuming Board is a Mongoose model
+    if (!board) {
+      return res.status(404).json({ message: "Board not found" });
+    }
+
+    const { board_name, ipaddress } = board;
+
+    const { temperature, date } = latestTemperature;
+
+    const response = {
+      board_name,
+      ipaddress,
+      board_id,
+      temperature,
+      date,
+    };
+    return res.status(200).json(response);
+  } catch (error) {
+    console.error("Error fetching humidity value:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const AddTempurateSensorValue = async (req, res, next) => {
+  try {
+    const { temperature, humidity, board_id } = req.body;
+
+    const board = await Board.findOne({ _id: board_id });
+
+    if (!board) {
+      return res.status(404).json({ success: false, error: "Board not found" });
+    }
+
+    const { board_name, ipaddress } = board;
+    const currentTime = new Date();
+    const offset = 7 * 60;
+    currentTime.setMinutes(currentTime.getMinutes() + offset);
+    const temperatureSensor = new TemperatureSensor({
+      board_name,
+      ipaddress,
+      board_id,
+      temperature,
+      humidity,
+      date: currentTime, // Assign current time
+    });
+
+    // Save the TemperatureSensor document to the database
+    await temperatureSensor.save();
+
+    return res.status(200).json({ success: true, sensor: temperatureSensor });
+  } catch (error) {
+    console.error("Error adding temperature and humidity sensor value:", error);
+    return res
+      .status(500)
+      .json({ success: false, error: "Internal server error" });
+  }
+};
+
+export const getAllDataT = async (req, res, next) => {
+  try {
+    const allData = await TemperatureSensor.find();
+    return res.status(200).json(allData);
+  } catch (error) {
+    console.error("Error fetching all data:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
